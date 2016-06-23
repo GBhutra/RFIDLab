148d0f9429a376aa65ea006d94d588f148d0d7ae
Ghanshyam Bhutra <mail.gbhutra@gmail.com>
2016-06-23 17:17:23 -0500

    reader module

--- rfid_app/index.js
+++ rfid_app/index.js
@@ -1,8 +1,16 @@
+'use strict';
+
+var llrp = require('./node_modules/llrp/LLRPMain.js');
 var express = require("express");
 var app = express();
 var port = 3700;
-var assets = [0];
-console.log("asset size="+assets.length);
+var assets = [0,1,2];
+var reader = new llrp({
+    ipaddress: '192.168.1.50',
+    isReaderConfigSet: false,
+    log: true,
+    isStartROSpecSent : false
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
@@ -17,22 +25,45 @@ io.sockets.on('connection', function (socket) {
     //socket.emit('message', { message: 'welcome to the chat' });
     console.log("message sent to the window")
     socket.on('start', function (data) {
+        reader.connect();
         socket.emit('message',data);
     });
     socket.on('stop', function (data) {
+        reader.disconnect();
         socket.emit('message',data);
     });
     socket.on('save', function (data) {
         socket.emit('message',data);
     });
     socket.on('layout', function (data) {
-        assets=[0,1];
+        if(3==assets.length)
+            assets=[1];
+        else if(2==assets.length)
+            assets=[0,1,2];
+        else if(1==assets.length)
+            assets=[0,1];
         console.log("asset size="+assets.length);
         socket.emit('message',data);
     });
     socket.on('exit', function (data) {
         socket.emit('message',data);
     });
+});
+
+reader.on('timeout', function () {
+    console.log('timeout');
+});
+
+reader.on('disconnect', function () {
+    console.log('disconnect');
+});
+
+reader.on('error', function (error) {
+    console.log('error: ' + JSON.stringify(error));
+});
+
+reader.on('didSeeTag', function (tag) {
+    console.log('TAG: ' + tag.tagID);
 });
 
 console.log("Listening on port " + port);
